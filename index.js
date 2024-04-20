cf871e75f9e23a85d7de2ca967456396b393b78e
Raju Achhami <rajachhami55@gamil.com>
2024-04-20 16:41:59 +0545

    added first file

---
+++ index.js
@@ -0,0 +1,36 @@
+
+import express from 'express';
+import productRoutes from './routes/productRoutes.js';
+const port = 5000;
+const app = express();
+
+
+
+app.get('/', (req, res)=>{
+    return res.status(200).json({
+        msg: 'welcome to my server'
+    });
+
+});
+
+
+
+
+app.use(productRoutes);
+
+
+
+// const handleReqRes = (req, res, next) =>  {
+//     console.log('hello je middleware');
+//     next();
+// }
+
+// app.use(handleReqRes);
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log('server live');
+})
