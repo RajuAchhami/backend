a0bbfca3ed3e9500d39a3f0bd489227fd5cad054
Raju Achhami <rajachhami55@gamil.com>
2024-04-20 23:34:47 +0545

    ch

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,43 @@
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+
+export const userLogin = (req, res) => {
+
+  return res.status(200).json({
+    status: 'success',
+    data: 'successfully logged in',
+    obj: req.body
+  });
+}
 
 
 
+export const userRegister = async (req, res) => {
+  const { email, password, fullname } = req.body;
+  try {
+    const isExist = await User.findOne({ email: email });
 
-
-export const userlogin = (req, res)=>{
-    console.log(req.body);
-    return res.status(200).json({
-        status : 'success',
-        data: 'Successfull loged In' 
+    if (isExist) return res.status(400).json({
+      status: 'error',
+      data: 'user already exist',
     });
-};
+
+    const hash = bcrypt.hashSync(password, 10);
+    await User.create({
+      email,
+      password: hash,
+      fullname
+    });
+    return res.status(200).json({
+      status: 'success',
+      data: 'successfully user registered',
+    });
+
+  } catch (err) {
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
+  }
+
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,32 @@
 import express from 'express';
-import { userlogin } from '../controllers/userController.js';
+import { userLogin, userRegister } from '../controllers/userController.js';
+import Joi from 'joi';
+import validator from 'express-joi-validation';
 
-const handleAll = (req, res)=>{
-    return res.status(405).json({
-        status : 'error',
-        messege : 'method not allowed'
-    });
+const validate = validator.createValidator({});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
+const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  fullname: Joi.string().min(5).max(20).required()
+});
+
+const handleAll = (req, res) => {
+  return res.status(405).json({
+    status: 'error',
+    message: 'method not allowed'
+  });
 }
 
 const router = express.Router();
 
+router.route('/login').post(validate.body(loginSchema), userLogin).all(handleAll);
+router.route('/register').post(validate.body(registerSchema), userRegister).all(handleAll);
 
-router.route('/login').post(userlogin).all(handleAll);
 
 
 export default router;
